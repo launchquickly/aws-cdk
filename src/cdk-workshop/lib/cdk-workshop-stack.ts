1fbd45e833abc3b727d344b44fd7e93699fa93be
pauljkearney <hello@launchquickly.scot>
2021-02-07 23:01:33 +0000

    Add Hello Lambda section

--- src/cdk-workshop/lib/cdk-workshop-stack.ts
+++ src/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,8 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
   }
 }
