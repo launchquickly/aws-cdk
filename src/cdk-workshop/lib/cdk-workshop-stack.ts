c047a54e612860f27d2f9855d7c3eb1227e1f79a
pauljkearney <hello@launchquickly.scot>
2021-02-07 22:29:11 +0000

    Cleaned up sample

--- src/cdk-workshop/lib/cdk-workshop-stack.ts
+++ src/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -7,12 +7,5 @@ export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
